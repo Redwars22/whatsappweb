b659cbc5a90c57700c37e92ad2a0558111908353
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-10-01 11:24:21 -0300

    2 files modified

--- src/components/LeftBar/LeftBarHeader.component.tsx
+++ src/components/LeftBar/LeftBarHeader.component.tsx
@@ -1,9 +1,11 @@
 import { HeaderButtonComponent } from '../LeftBar/HeaderButton.component';
 import { ProfilePictureComponent } from '../ProfilePicture/ProfilePicture.component';
 
+import './style.css';
+
 export function LeftBarHeaderComponent() {
   return (
-    <>
+    <div className={'header'}>
       <ProfilePictureComponent source={'https://github.com/Redwars22.png'} />
       <div>
         <HeaderButtonComponent
@@ -14,6 +16,6 @@ export function LeftBarHeaderComponent() {
           title={'aaaaaaaa'}
         />
       </div>
-    </>
+    </div>
   );
 }
