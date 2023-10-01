71ef81c9a9b9098e030d56c4309d610377dd4769
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-10-01 13:47:10 -0300

    1 file modified

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,4 @@ import { App } from './App';
 
 const root = createRoot(document.getElementById('app'));
 
-root.render(
-  <StrictMode>
-    <App name="StackBlitz" />
-  </StrictMode>
-);
+root.render(<App name="WhatsAppWebClone" />);
