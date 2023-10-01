d89c1343bb6fa9362ce6148fe3110641da354ca1
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-10-01 11:02:12 -0300

    1 file modified, 1 added, 1 deleted

--- src/components/ProfilePicture/ProfilePicture.component.tsx
+++ src/components/ProfilePicture/ProfilePicture.component.tsx
@@ -1,6 +1,6 @@
 import IProfilePicture from '../../types/ProfilePicture';
 
-import * as styles from './ProfilePicture.style.scss';
+import * as styles from './ProfilePicture.style.sass';
 
 export function ProfilePictureComponent(props: IProfilePicture) {
   return <img className={styles.ProfilePictures} src={props.source} />;
