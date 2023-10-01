0e13ca8b027fcc737f23509704d75f2a746e46fd
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-10-01 11:46:18 -0300

    2 files modified

--- src/components/LeftBar/LeftBar.component.tsx
+++ src/components/LeftBar/LeftBar.component.tsx
@@ -1,9 +1,12 @@
 import { HeaderButtonComponent } from '../LeftBar/HeaderButton.component';
 import { LeftBarHeaderComponent } from '../LeftBar/LeftBarHeader.component';
+
+import './style.css';
+
 export function LeftBarComponent() {
   return (
-    <>
+    <div className="left-bar">
       <LeftBarHeaderComponent />
-    </>
+    </div>
   );
 }
