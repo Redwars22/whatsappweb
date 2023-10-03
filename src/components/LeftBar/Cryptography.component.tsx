5f8156da82515af5885f3a46764e7c0b073acfd9
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-10-02 21:00:33 -0300

    2 files modified

--- src/components/LeftBar/Cryptography.component.tsx
+++ src/components/LeftBar/Cryptography.component.tsx
@@ -1,11 +1,11 @@
 export function CryptographyComponent() {
   return (
-    <>
+    <div className="cryptography-notice">
       <i className="bi bi-lock-fill" />
       <span>
         Suas mensagens pessoais são protegidas com a{' '}
         <a href="/">criptografia de ponta a ponta</a>
       </span>
-    </>
+    </div>
   );
 }
