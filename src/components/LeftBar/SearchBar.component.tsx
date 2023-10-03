46bb78d2b8f1b0752801d92c8ba97b05b20581e7
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-10-02 21:02:29 -0300

    1 file modified

--- src/components/LeftBar/SearchBar.component.tsx
+++ src/components/LeftBar/SearchBar.component.tsx
@@ -3,7 +3,7 @@ export function SearchBarComponent() {
     <div className="search-bar-container">
       <div className="search-bar-component">
         <i className="bi bi-search"></i>
-        <input />
+        <input placeholder="Pesquisar ou começar uma nova conversa" />
       </div>
       <button>
         <i className="bi bi-filter"></i>
