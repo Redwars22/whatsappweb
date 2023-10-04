eee796b3f8f8fa649790adefba5cd8b76e1d67be
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-10-04 10:27:09 -0300

    1 file modified

--- src/components/ChatBubbles/ChatBubble.component.tsx
+++ src/components/ChatBubbles/ChatBubble.component.tsx
@@ -1,12 +1,14 @@
 import { IChatBubble } from '../../types/chat';
+import { ReadStatusIcon } from '../ReadStatusIcon/ReadStatusIcon.component';
 
 import './style.css';
 
 export function ChatBubbleComponent(props: IChatBubble) {
   return (
-    <div className={`chat-bubble ${props.isSender ? 'white' : 'green'}`}>
-      <span>{props.message}</span>
-      <span className={'timestamp'}>{props.timeStamp}</span>
+    <div className={`chat-bubble ${props?.isSender ? 'white' : 'green'}`}>
+      <span>{props?.message}</span>
+      {!props?.isSender && <ReadStatusIcon status={props?.status} />}
+      <span className={'timestamp'}>{props?.timeStamp}</span>
     </div>
   );
 }
