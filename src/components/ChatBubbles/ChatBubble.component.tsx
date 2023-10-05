895382f184b0f388d821eed4c959bc1fe04ae61b
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-10-05 10:55:00 -0300

    2 files modified

--- src/components/ChatBubbles/ChatBubble.component.tsx
+++ src/components/ChatBubbles/ChatBubble.component.tsx
@@ -5,13 +5,22 @@ import './style.css';
 
 export function ChatBubbleComponent(props: IChatBubble) {
   return (
-    <div className="chat-bubble-container">
-      <i className="material-icons">sentiment_very_satisfied</i>
+    <div
+      className={`chat-bubble-container ${
+        props?.isSender ? 'left-margin' : 'right-margin'
+      }`}
+    >
+      {!props.isSender && (
+        <i className="material-icons">sentiment_very_satisfied</i>
+      )}
       <div className={`chat-bubble ${props?.isSender ? 'white' : 'green'}`}>
         <span>{props?.message}</span>
         <span className={'timestamp'}>{props?.timeStamp}</span>
         {!props?.isSender && <ReadStatusIcon status={props?.status} />}
       </div>
+      {props.isSender && (
+        <i className="material-icons">sentiment_very_satisfied</i>
+      )}
     </div>
   );
 }
