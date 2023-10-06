5ba01d9a8eabafb282fddb74e9dd74686f50e13a
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-10-06 14:55:43 -0300

    1 file modified

--- src/context/ConversationContext.tsx
+++ src/context/ConversationContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext } from 'react';
+import { createContext, useState, useContext } from 'react';
 
 // Create the context
 export const ConversationContext = createContext<{
